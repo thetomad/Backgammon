84e6547f34fcd5cd1a5ef906f8efae5642d681f5
Toma Dragomir <tomadragomir1@gmail.com>
2022-05-07 17:18:58 +0300

    Created functions for peaces moving

--- Python/backgammon.py
+++ Python/backgammon.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+
+def p_moves(dice, positions, turn):
+    f_pos = input(f"Your dice was {dice}: what position is the chip you want to move to? ")
+
+    f_pos = int(f_pos) -1
+
+    if turn == 1:
+        fin_pos = f_pos - dice
+    else: 
+        fin_pos = f_pos + dice
+
+
+    if positions[f_pos][1] == turn:
+        print("You can move from here.") 
+        if positions[f_pos][0] >=0:
+            print("You have a peace here.")
+            
+            if f_pos - dice > 0:
+                print("The place is on the board")
+                if positions[fin_pos][1] == turn or positions[fin_pos][1] == 0:
+                    print("The place you end is empty/yours.")
+                    positions[f_pos][0] += -1
+                    positions[fin_pos][0] += 1
+
+                    if positions[fin_pos][1] == 0:
+                        positions[fin_pos][1] = turn
+
+
+                    if positions[f_pos][0] == 0:
+                        positions[f_pos][1] =0
+
+
+
 def main():
     positions = []
 
@@ -17,6 +50,8 @@ def main():
     positions[16] = [3, -1]
     positions[18] = [5, -1]
     positions[23] = [2, 1]
+    
+    
     for i in range(24):
         print(f"{positions[i]}  --  pos{i + 1}")
 
@@ -31,54 +66,19 @@ def main():
 
     print(f"Your dices were {dice1} and {dice2}. \n")
 
-    f_pos = input(f"Your first dice was {dice1}: what position is the chip you want to move to? ")
 
-    f_pos = int(f_pos) -1
+    #first peaces moves
+    p_moves(dice1, positions, turn)
 
-   
-    if positions[f_pos][1] == turn:
-        print("You can move from here.") 
-        if positions[f_pos][0] >=0:
-            print("You have a peace here.")
-            
-            if f_pos - dice1 > 0:
-                print("The place is on the board")
-                if positions[f_pos - dice1][1] == turn or positions[f_pos - dice1][1] == 0:
-                    print("The place you end is empty/yours.")
-                    positions[f_pos][0] += -1
-                    positions[f_pos - dice1][0] += 1
+    #second peace moves
 
-                    if positions[f_pos - dice1][1] == 0:
-                        positions[f_pos - dice1][1] = turn
+    p_moves(dice2, positions, turn)
 
-
-                    if positions[f_pos][0] == 0:
-                        positions[f_pos][1] =0
-
-    s_pos = input(f"Your first dice was {dice2}: what position is the chip you want to move to? ")
-
-    s_pos = int(s_pos) -1
-
-   
-    if positions[s_pos][1] == turn:
-        print("You can move from here.") 
-        if positions[s_pos][0] >=0:
-            print("You have a peace here.")
-            
-            if s_pos - dice2 > 0:
-                print("The place is on the board")
-                if positions[s_pos - dice2][1] == turn or positions[s_pos - dice2][1] == 0:
-                    print("The place you end is empty/yours.")
-                    positions[s_pos][0] += -1
-                    positions[s_pos - dice2][0] += 1
-
-                    if positions[s_pos - dice2][1] == 0:
-                        positions[s_pos - dice2][1] = turn
-
-
-                    if positions[s_pos][0] == 0:
-                        positions[s_pos][1] =0
-
+    # turn ends
+    if turn == 1:
+        turn = -1
+    else:
+        turn == 1
 
     print(positions)
     
